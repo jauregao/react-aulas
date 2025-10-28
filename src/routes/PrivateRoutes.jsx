123a8fccc447d1829d0491639e0d058181658063
Amanda Jauregui <amandajauregui.dev@gmail.com>
2025-10-28 15:06:28 -0300

    atualização do código da aula 2

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -3,9 +3,9 @@ import { Navigate } from "react-router-dom";
 export function PrivateRoutes({ children }) {
   const token = localStorage.getItem("token");
 
-  // if (!token) {
-  //   return <Navigate to="/meuapp/home" />;
-  // }
+  if (!token) {
+    return <Navigate to="/meuapp/home" />;
+  }
 
   return children;
 }
